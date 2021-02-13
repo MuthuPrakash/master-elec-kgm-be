92a43a35c51ecee47fa34ff03674fe10fb031d65
Muthu <muthu167@gmail.com>
2021-02-13 10:26:45 +0530

    Initial commit with two routes working

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var MongoClient = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var bodyParser= require('body-parser')
+// Connect to the db
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+
+    MongoClient.connect("mongodb+srv://masterelectricals:Masterelectricals@cluster0.1dhzp.mongodb.net/sampledb?retryWrites=true&w=majority", { useNewUrlParser: true }, (error, client) => {
+        if (error) {
+            throw error;
+        }
+        database = client.db("sampledb");
+        collection = database.collection("samplecoll");
+        console.log("Connected to db!");
+    });
+
+});
+
+app.get("/ping", (req, res, next) => {
+    res.json("pong");
+});
+
+app.post("/api/items", (request, response) => {
+     collection.findOneAndUpdate({bookid: request.body.bookid}, {$set: request.body}, {upsert: true}, (error, result) => {
+        console.log('request.body: ', request.body)
+        if (error) {
+            return response.status(500).json(error);
+        }
+        console.log('result: ', result);
+        response.status(200).json(result);
+    });
+});
+
+app.get("/api/items", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if (error) {
+            return response.status(500).json(error);
+        }
+        console.log(result);
+        response.send(result);
+    });
+});
